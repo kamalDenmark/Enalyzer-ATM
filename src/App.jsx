a204689108663d1b6a2273db3c2b6b67643f52ba
kamalDenmark <mmk.denmark@gmai.com>
2024-08-01 11:48:15 +0200

    home page added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,27 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [page, setPage] = useState("home");
 
   return (
     <div className="App">
-      <h1>Enalyzer-ATM</h1>
+      <div className="container">
+        <div className="Appheading">
+          Enalyzer <br /> ATM
+        </div>
+        <div
+          className="Appbutton button"
+          onClick={() => setPage("withdraw")}
+          style={{
+            background: page === "withdraw" ? "white" : "",
+            color: page === "withdraw" ? "#2bbed3" : "",
+          }}
+        >
+          Withdraw
+        </div>
+        {/* <div className="button">Deposit</div> */}
+      </div>
     </div>
   );
 }
